ca9a1c09bac32496224c2643dd2c10edc650ad5e
Damion Armentrout <damion.armentrout@phoenix.edu>
2017-05-17 12:25:47 -0700

    file clean-up

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // get all animals or just the ones from the search
 $('#submit0').on('click keydown', function(e){
 	e.preventDefault();
-	$.get('mm1.php', $('#form0').serialize(), function(data){
+	$.get('mm.php', $('#form0').serialize(), function(data){
 		animals = $.parseJSON(data);
 		$('#theResults0').html("");
 		for(i=0;i<animals.length;i++){
@@ -28,7 +28,7 @@ $('#submit0').on('click keydown', function(e){
 // match animals to the search term
 $('#submit1').on('click keydown', function(e){
 	e.preventDefault();
-	$.get('mm1.php', $('#form1').serialize(), function(data){
+	$.get('mm.php', $('#form1').serialize(), function(data){
 		var epithet = $('#find_match').val();
 		animals = $.parseJSON(data);
 		$('#theResults1').html("");
